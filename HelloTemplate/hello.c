034cc03a023114bec8dfee124f3370e9fc068a98
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-27 20:25:00 +0530

    feat: add hello world template

---
+++ HelloTemplate/hello.c
@@ -0,0 +1,12 @@
+/**
+  * @author      : Linus Fernandes (linusfernandes at gmail dot com)
+  * @file        : hello
+  * @created     : Saturday May 27, 2023 20:18:17 IST
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
