2e557f058eee6b14d30aecc7f109a019a915374f
Linus Fernandes <linus.fernandes@gmail.com>
2023-05-27 18:47:15 +0530

    feat: add hello world

---
+++ hello.c
@@ -0,0 +1,12 @@
+/**
+  * @author      : Linus Fernandes (linusfernandes at gmail dot com)
+  * @file        : hello
+  * @created     : Saturday May 27, 2023 18:37:37 IST
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
