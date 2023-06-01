22120526052a5cc27cab5da755fe0513471192d2
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-01 11:11:14 +0530

    feat: add memory example

--- WasiHelloWorld/target/debug/deps/WasiHelloWorld-b9ab2a614b284e81.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/arch/root/LearnWebAssembly/WasiHelloWorld/target/debug/deps/WasiHelloWorld-b9ab2a614b284e81.rmeta: src/main.rs
-
-/data/data/com.termux/files/home/arch/root/LearnWebAssembly/WasiHelloWorld/target/debug/deps/WasiHelloWorld-b9ab2a614b284e81.d: src/main.rs
-
-src/main.rs:
--- WasiHelloWorld/target/wasm32-wasi/debug/deps/WasiHelloWorld-c72eef5d642800e8.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/arch/root/LearnWebAssembly/WasiHelloWorld/target/wasm32-wasi/debug/deps/WasiHelloWorld-c72eef5d642800e8.wasm: src/main.rs
-
-/data/data/com.termux/files/home/arch/root/LearnWebAssembly/WasiHelloWorld/target/wasm32-wasi/debug/deps/WasiHelloWorld-c72eef5d642800e8.d: src/main.rs
-
-src/main.rs:
