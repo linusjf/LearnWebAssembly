f4c6d324fd468189d46e7efe53daac58ed4b5133
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-02 13:15:40 +0530

    feat: add Game of Life

---
+++ GameOfLife/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+######################################################################
+# @author      : Linus Fernandes (linusfernandes at gmail dot com)
+# @file        : build
+# @created     : Wednesday May 31, 2023 08:13:31 IST
+#
+# @description :
+######################################################################
+npm run asbuild
