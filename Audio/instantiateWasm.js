21b22908b1b37e4e9a63e7f43d92d18085a813b9
Linus Fernandes <linus.fernandes@gmail.com>
2023-06-05 14:46:53 +0530

    feat: add audio example

---
+++ Audio/instantiateWasm.js
@@ -0,0 +1,34 @@
+/**
+* @class       : instantiateWasm
+* @author      : Linus Fernandes (linusfernandes at gmail dot com)
+* @created     : Monday Jun 05, 2023 14:42:03 IST
+* @description : instantiateWasm
+*/
+export const wasmBrowserInstantiate = async (wasmModuleUrl, importObject) => {
+  let response = undefined;
+
+  if (!importObject) {
+    importObject = {
+      env: {
+        abort: () => console.log("Abort!")
+      }
+    };
+  }
+
+  if (WebAssembly.instantiateStreaming) {
+    response = await WebAssembly.instantiateStreaming(
+      fetch(wasmModuleUrl),
+      importObject
+    );
+  } else {
+    const fetchAndInstantiateTask = async () => {
+      const wasmArrayBuffer = await fetch(wasmModuleUrl).then(response =>
+        response.arrayBuffer()
+      );
+      return WebAssembly.instantiate(wasmArrayBuffer, importObject);
+    };
+    response = await fetchAndInstantiateTask();
+  }
+
+  return response;
+};
